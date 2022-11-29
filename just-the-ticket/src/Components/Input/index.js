e4b8cb35f4298c9cef6042fe09b0e7b9da32b05b
Callum <84125856+callum-cheshire@users.noreply.github.com>
2022-11-29 14:05:07 +0000

    added jsdocs for Input component

--- just-the-ticket/src/Components/Input/index.js
+++ just-the-ticket/src/Components/Input/index.js
@@ -1,3 +1,17 @@
+/**
+ * 
+ * @param {string} type 
+ * @param {string} className 
+ * @param {string} autoComplete
+ * @param {string} placeholder
+ * @param {string} name
+ * @param {string} id
+ * @param {function} onChange
+ * @param {string} value
+ * @param {string} required
+ * @returns Component containing div with className={containerClassName}, which contains a label and an input
+ */
+
 const Input = ({handleChange, htmlName, type, inputClassName, autoComplete, placeholder, value, containerClassName, label}) => {
   return ( 
     <div className={containerClassName}>
